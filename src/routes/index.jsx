90919400cbbe1af65e351bc835c15ee17fb7954e
matheus55391 <matheus.manulo55@gmail.com>
2022-05-21 14:19:40 -0300

    fix router bug

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,20 +1,17 @@
-import { Routes, Route, Navigate, Outlet } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { Chat, Login } from '../pages'
 import { useAuth } from '../shared/contexts/AuthContext'
 
+
 export const AppRoutes = () => {
 	const { usuario } = useAuth()
-
-	const PrivateRoute = () =>{
-		return usuario ? <Outlet /> : <Navigate to="/MeguChat/login" />
+	const RenderChat = () =>{
+		return usuario? <Chat/> : <Navigate to="/MeguChat/login" />
 	}
-
 	return (
 		<Routes>			
 			<Route path='/MeguChat/login' element={<Login/>}/>
-			<Route element={<PrivateRoute/>}>
-				<Route path='/MeguChat/chat' element={<Chat/>}/>
-			</Route>
+			<Route path='/MeguChat/chat' element={<RenderChat/>}/>
 			<Route path='*' element={<Navigate to="/MeguChat/login" />}/>
 		</Routes>
 	)
