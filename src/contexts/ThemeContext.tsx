e9c367ad242d8d71ec67bd22f86003997f5a74eb
matheus55391 <matheus.manulo55@gmail.com>
2024-03-03 19:39:23 -0300

    fix/corrigido erro localstorage not defined

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 enum Theme {
     LIGHT = 'light',
@@ -18,13 +18,24 @@ interface ThemeProviderProps {
 }
 
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
-    const [theme, setTheme] = useState<Theme>(() => {
-        const storedTheme = localStorage?.getItem('theme');
-        return storedTheme === Theme.DARK ? Theme.DARK : Theme.LIGHT;
-    });
-
+    const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+    const [mounted, setMounted] = useState(false);
 
     useEffect(() => {
+        if (mounted) {
+            return
+        }
+        const storedTheme = localStorage?.getItem('theme');
+        console.log("storedTheme", storedTheme)
+        setTheme(storedTheme === Theme.DARK ? Theme.DARK : Theme.LIGHT);
+        setMounted(true)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => {
+        if (!mounted) {
+            return
+        }
         if (theme === Theme.DARK) {
             document.documentElement.classList.add('dark');
             localStorage?.setItem('theme', Theme.DARK);
@@ -32,7 +43,7 @@ export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
             document.documentElement.classList.remove('dark');
             localStorage?.setItem('theme', Theme.LIGHT);
         }
-    }, [theme]);
+    }, [theme, mounted]);
 
     const toggleTheme = () => {
         setTheme((prevTheme) => (prevTheme === Theme.DARK ? Theme.LIGHT : Theme.DARK));
